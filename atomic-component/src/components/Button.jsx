b109d498cc90088e44cf8e6c855da9306c71fb2d
YSP97 <pus9717@gmail.com>
2024-07-28 16:56:13 +0900

    [Fix] Button 타입 추가

--- atomic-component/src/components/Button.jsx
+++ atomic-component/src/components/Button.jsx
@@ -12,6 +12,7 @@ export default function Button({ status, onClick }) {
 
   return (
     <button
+      type="button"
       className={`button ${status}`}
       onClick={onClick}
       aria-label={statusMessage}>
