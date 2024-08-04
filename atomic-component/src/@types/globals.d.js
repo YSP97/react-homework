6351e306477c282c2209db114c05dcdd4180386f
YSP97 <pus9717@gmail.com>
2024-08-04 21:20:21 +0900

    [수정] statusType export

--- atomic-component/src/@types/globals.d.js
+++ atomic-component/src/@types/globals.d.js
@@ -1,6 +1,6 @@
 import { string, number, oneOf, exact } from 'prop-types';
 
-const statusType = oneOf(['lock', 'edit']);
+export const statusType = oneOf(['lock', 'edit']);
 
 export const profileType = exact({
   name: string.isRequired,
