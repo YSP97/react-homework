9f07f02cb9eebbd30437bf670307328dd01a7b60
YSP97 <pus9717@gmail.com>
2024-08-16 20:44:09 +0900

    [마크업] To-Do-List App 만들기 - Button 컴포넌트 마크업, 스타일링

---
+++ to-do-list-app/src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import S from './Button.module.css';
+function Button() {
+  return (
+    <div className={S.component}>
+      <button type="button">생각 났어?</button>
+    </div>
+  );
+}
+
+export default Button;
