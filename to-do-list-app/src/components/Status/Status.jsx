3000703c9b8f87de4edb44e70c83832a25284b2d
YSP97 <pus9717@gmail.com>
2024-08-17 20:01:45 +0900

    [기능구현] To-Do-List App 만들기 - 클릭시 status 변경

--- to-do-list-app/src/components/Status/Status.jsx
+++ to-do-list-app/src/components/Status/Status.jsx
@@ -1,7 +1,12 @@
 import S from './Status.module.css';
-function Status({ count, title, isActive }) {
+function Status({ count, title, onClick, status, isActive }) {
+  const handleClick = (e) => {
+    e.preventDefault();
+    onClick?.(status);
+  };
+
   return (
-    <div className={S.component}>
+    <div className={S.component} onClick={handleClick}>
       <a href="/" className={`${S.link} ${isActive ? S.isActive : ''}`}>
         <p>{title}</p>
         <div>{count}</div>
--- to-do-list-app/src/components/StatusBar/StatusBar.jsx
+++ to-do-list-app/src/components/StatusBar/StatusBar.jsx
@@ -1,21 +1,7 @@
-import { useState } from 'react';
 import Status from '../Status/Status';
 import S from './StatusBar.module.css';
-function StatusBar() {
-  const data = [
-    { title: '모두', count: 3 },
-    { title: '한일', count: 1 },
-    { title: '할일', count: 2 },
-    { title: '보관', count: 1 },
-  ];
 
-  const [isActive, setIsActive] = useState(false);
-
-  const handleStatusClick = () => {
-    setIsActive(true);
-    console.log(isActive);
-  };
-
+function StatusBar({ data, onStatusClick, status, list }) {
   return (
     <div className={S.component}>
       {data.map((item, index) => {
@@ -25,8 +11,9 @@ function StatusBar() {
               key={index}
               title={item.title}
               count={item.count}
-              isActive={isActive}
-              onClick={() => handleStatusClick(item.title)}
+              status={item.status}
+              isActive={status === item.status}
+              onClick={onStatusClick}
             />
             {index === 0 ? <p>|</p> : ''}
           </>
